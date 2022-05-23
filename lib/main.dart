a4b15c583fcd7bdb82991412e7280a95bef76bbc
Ajai Olufisayo <tadeajai4@gmail.com>
2022-05-23 01:54:51 +0100

    more changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,94 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
-import './transaction.dart';
+import 'package:peronal_expenes_app/transaction.dart';
 
-void main() {
-  runApp(MyHomePage());
-}
+void main() => runApp(const MyApp());
 
-class MyHomePage extends StatelessWidget {
-  final List transactions = <Transaction>[
-    Transaction(id: 1, title: "New Shoes", amount: 69.99, time: DateTime.now())
-  ];
-
-  MyHomePage({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter App'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          textDirection: TextDirection.rtl,
-          children: const [
-            SizedBox(
-              width: double.infinity,
-              child: Card(
-                child: Text("chart"),
-                elevation: 5,
-              ),
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final transactions = [
+    Transaction(
+        id: 01, title: "New Shoes", amount: 10000.0, time: DateTime.now()),
+    Transaction(
+        id: 02, title: "Weekly Grocery", amount: 4000.0, time: DateTime.now()),
+  ];
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: const Card(
+              color: Colors.blue,
+              child: Text('CHART!'),
+              elevation: 5,
             ),
-            Card(
-              child: Text("List of transactions"),
-            )
-          ],
-        ),
+          ),
+          Column(
+            children: transactions.map((transaction) {
+              return Card(
+                child: Container(
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 10),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.purple,
+                                width: 2,
+                                style: BorderStyle.solid)),
+                        child: Text('₦${transaction.amount}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple,
+                            )),
+                      ),
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              transaction.title,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 15),
+                            ),
+                            Text(
+                              transaction.time.toString(),
+                              style: TextStyle(color: Colors.grey),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
